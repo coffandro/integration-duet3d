7080e36101a73774d3f845bfc5daa73826ad6a57
Tim Schneider <tim@schneider.engineering>
2025-01-13 17:00:37 +0100

    check if dist folder exists, then remove content

--- create-package.sh
+++ create-package.sh
@@ -5,7 +5,9 @@ set -e
 source yapf-module.sh
 source flake8-module.sh
 
-rm dist/*
+if [ -d "dist" ]; then
+    rm dist/*
+fi
 pip install --upgrade build
 python3 -m build
 #python3 setup.py bdist_wheel
